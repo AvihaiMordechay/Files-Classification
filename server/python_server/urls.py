657a54b89db9780cd3275fe142196d208a91288e
Avihai <avihaimo1@gmail.com>
2024-12-07 18:49:39 +0200

    add not found rest

--- server/python_server/urls.py
+++ server/python_server/urls.py
@@ -1,9 +1,16 @@
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
+from django.contrib import admin
+
+
+def not_found_view(request):
+    return HttpResponse("This page does not exist. Please use one of the defined paths.", status=404)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('parsing_text.urls')),
     path('model/', include('machine_learning.urls')),
     path('csrf/', include('csrf_handler.urls')),
+    path('', not_found_view),
 ]
