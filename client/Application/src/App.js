7771f10fcfe32d8982894ef908487163d0ec67ab
Avihai <avihaimo1@gmail.com>
2025-01-30 13:46:50 +0200

    init the component for start

--- client/Application/src/App.js
+++ client/Application/src/App.js
@@ -2,13 +2,15 @@ import { StyleSheet } from 'react-native';
 import Navigator from './navigation/Navigator';
 import User from './user/user';
 import foldersCategories from '../assets/data/foldersCategories.json'
+import AddFiles from './components/AddFilesComponent';
 
 
 export default function App() {
   const user = new User("אביחי", "מרדכי", "male", "avihaimo1@gmail.com", require("../assets/profile.jpg"), foldersCategories);
   return (
     <>
-      <Navigator user={user} />
+      <AddFiles />
+      {/* <Navigator user={user} /> */}
     </>
   );
 }
