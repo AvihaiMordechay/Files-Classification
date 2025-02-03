2c136259f62d749c6b3cd8f639866429530ecba3
Avihai <avihaimo1@gmail.com>
2025-02-03 20:09:35 +0200

    Counting the registration screen and connect it to firebase auth

--- client/Application/src/App.js
+++ client/Application/src/App.js
@@ -1,14 +1,16 @@
-import { StyleSheet } from 'react-native';
 import Navigator from './navigation/Navigator';
 import User from './user/user';
 import foldersCategories from '../assets/data/foldersCategories.json'
-
+import AddFiles from './components/AddFilesComponent';
+import RegistrationScreen from './screens/RegistrationScreen';
 
 export default function App() {
   const user = new User("אביחי", "מרדכי", "male", "avihaimo1@gmail.com", require("../assets/profile.jpg"), foldersCategories);
   return (
     <>
-      <Navigator user={user} />
+      {/* <Navigator user={user} /> */}
+      {/* <AddFiles /> */}
+      <RegistrationScreen />
     </>
   );
 }
--- client/Application/src/services/firebase.js
+++ client/Application/src/services/firebase.js
@@ -1,19 +1,29 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';  // ייבוא AsyncStorage
+
+import {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID
+} from '@env';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { auth };
