5386ba4061853c05ba1f5ba6e4fd702647453ec0
Avihai <avihaimo1@gmail.com>
2025-02-10 00:18:01 +0200

    no message

--- client2/application/src/services/firebase.js
+++ client2/application/src/services/firebase.js
@@ -2,22 +2,22 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';  // ייבוא AsyncStorage
 
-// import {
-//     REACT_APP_FIREBASE_API_KEY,
-//     REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     REACT_APP_FIREBASE_PROJECT_ID,
-//     REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     REACT_APP_FIREBASE_APP_ID
-// } from '@env';
+import {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID
+} from '@env';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBdmN9_ELgy-Ui-EnLkosq8iE0cpIbh8Q4",
-    authDomain: "files-classification.firebaseapp.com",
-    projectId: "files-classification",
-    storageBucket: "files-classification.appspot.com",
-    messagingSenderId: "912043324894",
-    appId: "1:912043324894:web:e9b2a26533009f8b8e594f",
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
