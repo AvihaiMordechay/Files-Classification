d367514e829d8565d1df175f834b7936a92011c7
OMERPC\omerz <omerzamir80@gmail.com>
2025-04-20 18:00:26 +0300

    the real slim shady

--- client/Application/src/components/UploadFile.js
+++ client/Application/src/components/UploadFile.js
@@ -5,6 +5,7 @@ import FileUploadModal from './modals/FileUploadModal';
 import CreateFolderModal from './modals/CreateFolderModal';
 import CategoryListModel from './modals/CategoryListModel';
 import ChangeFileNameModel from './modals/ChangeFileNameModel';
+import Spinner from './Spinner';
 
 const UploadFile = ({ file }) => {
     const { user, createNewFolder } = useUser();
@@ -16,6 +17,7 @@ const UploadFile = ({ file }) => {
     const [categoryListModelVisible, setCategoryListModelVisible] = useState(false);
     const [changeFileNameModelVisible, setChangeFileNameModelVisible] = useState(false);
     const [folderId, setFolderId] = useState(null);
+    const [loading, setLoading] = useState(false);
 
 
     useEffect(() => {
@@ -88,6 +90,7 @@ const UploadFile = ({ file }) => {
 
     const uploadFileToServer = async (file) => {
         try {
+            setLoading(true);
             // Create form data
             const formData = new FormData();
             formData.append('file', {
@@ -103,7 +106,7 @@ const UploadFile = ({ file }) => {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-
+            setLoading(false);
             const responseText = await uploadResponse.text();
 
             if (uploadResponse.ok) {
@@ -126,11 +129,15 @@ const UploadFile = ({ file }) => {
 
     return (
         <>
+            {loading && (
+                <Spinner />
+            )}
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
                 <FileUploadModal
                     visible={newCategoryModalVisible}
                     content={`המערכת זיהתה מסמך מקטגוריה חדשה בשם: ${category}, מה ברצונך לעשות?`}
+                    buttons={[
                         {
                             text: 'צור תיקייה חדשה',
                             icon: 'add-outline',
@@ -144,7 +151,7 @@ const UploadFile = ({ file }) => {
                         {
                             icon: 'folder-outline',
                             isPrimary: true,
-                            text: צור תיקייה ${category},
+                            text: `צור תיקייה ${category}`,
                             onPress: () => handleButtonPress('createCategoryFolder', "newCategoryModal"),
                         },
                     ]}
@@ -153,7 +160,7 @@ const UploadFile = ({ file }) => {
 
                 <FileUploadModal
                     visible={existCategoryModalVisible}
-                    content={המערכת זיהתה מסמך מקטגורית: ${category}, מה ברצונך לעשות?}
+                    content={`המערכת זיהתה מסמך מקטגורית: ${category}, מה ברצונך לעשות?`}
                     buttons={[
                         {
                             text: 'צור תיקייה חדשה',
@@ -168,7 +175,7 @@ const UploadFile = ({ file }) => {
                         {
                             icon: 'save-outline',
                             isPrimary: true,
-                            text: שמור בתיקיית ${category},
+                            text: `שמור בתיקיית ${category}`,
                             onPress: () => handleButtonPress('saveToExistingCategory', "existCategoryModal"),
                         },
                     ]}
